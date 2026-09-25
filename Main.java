3caf2c84bb730fbc561c9dbdadb2786a2049e006
Yousof Abouhalawa <github@yousofabouhalawa.com>
2026-09-25 08:57:34 +0300

    feat: handle missing command-line argument

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Reading arguments: " + args[0]);
+        if (args.length > 0) {
+            System.out.println("Reading arguments: " + args[0]);
+        } else {
+            System.out.println("Nasaq expects at least one argument");
+        }
     }
 }
