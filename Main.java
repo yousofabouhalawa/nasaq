bfd30db5956d9e13be55452da65c3bdf7c99523d
Yousof Abouhalawa <github@yousofabouhalawa.com>
2026-09-25 09:12:59 +0300

    refactor: name command argument

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         if (args.length > 0) {
-            if (args[0].equals("start")) {
+            String command = args[0];
+            if (command.equals("start")) {
                 System.out.println("Nasaq starting...");
-            } else if (args[0].equals("status")) {
+            } else if (command.equals("status")) {
                 System.out.println("Status: healthy");
             } else {
-                System.out.println("Unknown command: " + args[0]);
+                System.out.println("Unknown command: " + command);
             }
         } else {
             System.out.println("Nasaq expects at least one argument");
