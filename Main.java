79f8f6944a91e3b71fda2e1782b4bf3939133d01
Yousof Abouhalawa <github@yousofabouhalawa.com>
2026-09-25 09:06:39 +0300

    feat: add basic command dispatch

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         if (args.length > 0) {
-            System.out.println("Reading arguments: " + args[0]);
+            if (args[0].equals("start")) {
+                System.out.println("Nasaq starting...");
+            } else if (args[0].equals("status")) {
+                System.out.println("Status: healthy");
+            } else {
+                System.out.println("Unknown command: " + args[0]);
+            }
         } else {
             System.out.println("Nasaq expects at least one argument");
         }
