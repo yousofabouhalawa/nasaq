45629057c8fe77d9d9639d1beea908b9e1da053a
Yousof Abouhalawa <github@yousofabouhalawa.com>
2026-09-25 15:47:59 +0300

    feat: manage multiple articles in memory

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
     static void executeCommand(String command) {
         if (command.equals("start")) {
             System.out.println("Nasaq starting...");
-            Article article = new Article("Article Title", "Article body");
-            System.out.println("Article title: " + article.getTitle() + "\n" + "Article body: " + article.getBody() + "\n" + "Is published: " + article.isPublished());
-            System.out.println("Publishing article...");
-            article.publish();
-            System.out.println("Is published: " + article.isPublished());
+            List<Article> articles = new ArrayList<>();
+            Article article1 = new Article("Article 1 Title", "Article 1 body");
+            Article article2 = new Article("Article 2 Title", "Article 2 body");
+            Article article3 = new Article("Article 3 Title", "Article 3 body");
+            articles.add(article1);
+            articles.add(article2);
+            articles.add(article3);
+            System.out.println(articles.size());
+
+            System.out.println("Article 1 title: " + articles.get(0).getTitle());
+            System.out.println("Article 2 title: " + articles.get(1).getTitle());
+            System.out.println("Article 3 title: " + articles.get(2).getTitle());
+            
         } else if (command.equals("status")) {
             System.out.println("Status: healthy");
         } else {
